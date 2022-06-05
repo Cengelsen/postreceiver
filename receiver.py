274627aca0aef3f028ad934052b6942a77ebdc19
Christian Engelsen <cengelsen@fribyte.no>
2022-06-05 20:00:43 +0200

    Secured code execution with secret keys

--- receiver.py
+++ receiver.py
@@ -1,36 +1,42 @@
 from flask import Flask, request
-import json, os, logging
+import json, logging, subprocess
 
 app = Flask(__name__)
 
 @app.route('/webhook',methods=['POST'])
 
+# Converts json payload to dictionary
+data = json.loads(request.data)
+
 # The function that handles the POST-request
 def deployWebsite():
-   data = json.loads(request.data)
    logmsg = "New commit by: {}".format(data['commits'][0]['author']['name'])
 
-   # Verifies if the method is POST, executes the commands and logs the event
+   # Verifies the payload, executes the commands and logs the event
    if request.method == 'POST':
-       os.system('cd /var/www/cengelsen.no && git pull && hugo && echo "Update has been deployed"')
+       verify_signature(data)
+       subprocess.call(['sh', '../build_site.sh'])
        logEvents(logmsg)
        return 'OK', 200
    else:
        abort(400)
 
 # function that defines how events are logged
-# currently, there are only one type of event; as defined by logmsg
 def logEvents(event):
 
-    # This defines the format of the log-event. 'a' means append, 
-    # which means the message is appended into the file POST.log
+    # This defines the format of the locally stored log-event. 
     logging.basicConfig(filename=r"/var/www/postreceiver/logs/POST.log", 
             format='%(asctime)s %(levelname)s %(message)s', filemode='a')
 
-    # defining the log-object that actually logs the message
+    # creates the log-object
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     logger.debug(event)
+
+# Githubs recommended function to verify JSON POST payloads
+def verify_signature(data)
+  signature = 'sha256=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE_256'])
 
 if __name__ == '__main__':
    app.run(debug=True)
