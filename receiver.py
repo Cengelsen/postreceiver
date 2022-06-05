e83551fffe0fa88585f92016150936ad6e16559c
Christian Engelsen <ckengelsen97@gmail.com>
2022-06-05 23:30:44 +0000

    if-else syntax rearranged

--- receiver.py
+++ receiver.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from hmac import HMAC, compare_digest
+from hashlib import sha256
 import json, logging, subprocess, os
 
 app = Flask(__name__)
@@ -16,11 +18,14 @@ def deployWebsite():
     # Verifies the payload, executes the commands and logs the event
     if request.method == 'POST':
        if verify_signature(data):
+           print("Secret token verified")
            subprocess.call(['sh', '../build_site.sh'])
            logEvents(logmsg)
            return 'Success', 200
-       return 'Signatures did not match!', 500
-    return 'Not allowed', 405
+       else:
+           return 'Signatures did not match!', 500
+    else:
+        return 'Not allowed', 405
 
 # function that defines how events are logged
 def logEvents(event):
@@ -36,9 +41,9 @@ def logEvents(event):
 
 # Githubs recommended function to verify JSON POST payloads
 def verify_signature(data):
-
-    received_sign = req.headers.get('X-Hub-Signature-256').split('sha256=')[-1].strip()
-    secret = os.environ(['SECRET_KEY']).encode()
+    print ("Verifying secret token...")
+    received_sign = data.headers.get('X-Hub-Signature-256').split('sha256=')[-1].strip()
+    secret = os.environ['SECRET_KEY']
     expected_sign = HMAC(key=secret, msg=data.data, digestmod=sha256).hexdigest()
     return compare_digest(received_sign, expected_sign)
 
