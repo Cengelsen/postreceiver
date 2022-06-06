3f952c64842cc7c6e091e341530f0c79ca0fc613
Christian Engelsen <ckengelsen97@gmail.com>
2022-06-06 21:54:43 +0000

    Working key comparison w/o correct signature verification

--- receiver.py
+++ receiver.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from hashlib import sha256
-import json, logging, subprocess, os, hmac
+from hmac import HMAC, compare_digest
+import json, logging, subprocess, os, hashlib
 
 app = Flask(__name__)
 
@@ -10,19 +11,21 @@ app = Flask(__name__)
 def deployWebsite():
     
     # Converts json payload to dictionary
-    data = json.loads(request.json)
-    head = json.loads(request.headers)
-    
+    data = json.loads(request.data)
+    head = request.headers
+
     logmsg = "New commit by: {}".format(data['commits'][0]['author']['name'])
    
     # Verifies the payload, executes the commands and logs the event
     if request.method == 'POST':
-       if verify_signature(data):
+       if verify_signature(data, head):
            print("Secret token verified")
            subprocess.call(['sh', '../build_site.sh'])
            logEvents(logmsg)
            return 'Success', 200
        else:
+           logmsg = "dette er header: " + head.get('X-Hub-Signature-256').strip()
+           logEvents(logmsg)
            return 'Signatures did not match!', 500
     else:
         return 'Not allowed', 405
@@ -40,13 +43,22 @@ def logEvents(event):
     logger.debug(event)
 
 
-def verify_signature(data):
-    print ("Verifying secret token...")
+def verify_signature(data, head):
     
-    received_sign = head['X-Hub-Signature-256']
-    #received_sign = data.headers.get('X-Hub-Signature-256').split('sha256=')[-1].strip()
-    secret = os.environ['SECRET_KEY'].encode()
-    expected_sign = 'sha256=' + HMAC(key=secret, msg=data.encode(), digestmod=hashlib.sha256).hexdigest()
+    received_sign = head.get('X-Hub-Signature-256').strip()
+
+    # Opens local json-file and converts to dictionary
+    with open('.env.local.json', 'r') as token:
+        secret = json.load(token)
+    
+    logmsg = "Dette er secret: " + secret['SECRET_TOKEN']
+    logEvents(logmsg)
+
+    # Generates a hexadecimal based on secret token and request payload
+    expected_sign = 'sha256=' + HMAC(key=bytes(secret['SECRET_TOKEN'], 'utf-8'), msg=bytes(request.data, 'utf-8'), digestmod=hashlib.sha256).hexdigest()
+
+    logmsg = "Dette er hmac: " + expected_sign
+    logEvents(logmsg)
     return compare_digest(received_sign, expected_sign)
 
 if __name__ == '__main__':
